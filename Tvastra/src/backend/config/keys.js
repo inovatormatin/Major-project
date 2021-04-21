7312d9448a7e840bc4e95be8658aaf76a4dc2dd9
Mohit <30345679+idk-Mohit@users.noreply.github.com>
2021-04-21 14:34:12 +0530

    Update keys.js

--- Tvastra/src/backend/config/keys.js
+++ Tvastra/src/backend/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://Mohit-Tvastra-123:mohittvastra123@tvastra.hbeqc.mongodb.net/Tvastra-Database?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://<Username here>:<password here>@tvastra.hbeqc.mongodb.net/Tvastra-Database?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
