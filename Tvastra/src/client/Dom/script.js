5bf928d3c55311c45f3d286033e4fdd703cb9a67
Mohit <mototurbo12499@gmail.com>
2021-04-22 13:26:15 +0530

    hello

--- Tvastra/src/client/Dom/script.js
+++ Tvastra/src/client/Dom/script.js
@@ -60,9 +60,3 @@ const showpass = () =>{
     pass.type = 'password';
   }
 }
-
-// var option2= document.getElementById("two");
-// function display() {
-//   console.log(option2);
-// }
-// option2.addEventListener("click", display);
